a92d052911aa1d9724d3d300dead1a2696b0ee4a
IvanMatejev <ivanmatejev@gmail.com>
2022-02-01 21:11:01 +0100

    insert symbol

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 let display=document.querySelector('.container');
+let symbol='O';
 
 showTicTac();
+
+let boxes=document.querySelectorAll('.boxxx');
+
+for(let i=0; i<boxes.length; i++){
+    const box2=boxes[i];
+    box2.addEventListener('click', insertSymbol);
+}
+
+
+
+function insertSymbol(){
+    if(symbol==='O'){
+        symbol='X'
+    }else{
+        symbol='O'
+    }
+    this.innerHTML=symbol;
+    this.removeEventListener('click', insertSymbol)
+}
 
 function showTicTac(){
     let box='';
